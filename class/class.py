e9b21610fb5babeb2f1709f118aa653e54c42ba8
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-10-18 13:28:56 +0600

    parameterized constructor(positional arguement helped to write multiple constructors

--- class/class.py
+++ class/class.py
@@ -2,5 +2,12 @@ class Student:
     def __init__(self):
         print(self)
         print("a student obj created")
+    def __init__(self, name, Id):
+        self.name = name
+        self.id = Id
 
-s1 = Student()
+#s1 = Student()
+s2 = Student("Bob", 11)
+s2.id = 33
+print(s2.name)
+print(s2.id)
