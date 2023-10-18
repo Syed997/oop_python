9d799ed7bc40de667d5548e1fc1393354a33116f
s.zim <syednur.jim@gmail.com>
2023-10-18 20:12:45 +0600

    more constructor practice with default attributes

--- class/class.py
+++ class/class.py
@@ -9,6 +9,24 @@ class Student:
         print("Name:",self.name, "ID:", self.id )
 
 #s1 = Student()
-s2 = Student("Bob", 11)
-s2.id = 33
-s2.details()
+#s2 = Student("Bob", 11)
+#s2.id = 33
+#s2.details()
+
+class House:
+    def __init__(self) -> None:
+        self.window = 4
+        self.door = 2
+
+    def view(self):
+        print("Windows:", self.window, "Doors:", self.door)
+
+h1 = House()
+h2 = House()
+
+h1.window = 6
+h1.door = 3
+h2.door = 4
+h2.window = 8
+h1.view()
+h2.view()
